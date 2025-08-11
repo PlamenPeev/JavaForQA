292d8147c89a7657934326d72b216c1d9ee782a6
Plamen Peev <velev13bg@abv.bg>
2025-08-11 23:19:59 +0300

    New project

---
+++ src/C05_Methods/P07_LastCharacterOfAString.java
@@ -0,0 +1,16 @@
+package C05_Methods;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class P07_LastCharacterOfAString {
+    public static void main(String[] args) {
+        String text = "Celebration";
+        lastCharacterOfAString(text);
+    }
+    public static void lastCharacterOfAString(String input){
+        char lastChar = input.charAt(input.length()-1);
+        //String lastChar = input.substring(input.length() - 1);
+        System.out.println("The last char is an text is: " + "'" + lastChar + "'");
+    }
+}
